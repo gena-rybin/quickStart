53af575114685c3d015f694178175d877115c286
Gennadiy Rybin <gennadiy.rybin@dataart.com>
2018-04-11 16:02:21 +0300

    works, before ang-material

--- src/app/angMaterial.module.ts
+++ src/app/angMaterial.module.ts
@@ -1,8 +1,8 @@
-import { NgModule }      from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-
-@NgModule({
-  imports: [MatButtonModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatCheckboxModule],
-})
-export class MyOwnCustomMaterialModule { }
+// import { NgModule }      from '@angular/core';
+// import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+//
+// @NgModule({
+//   imports: [MatButtonModule, MatCheckboxModule],
+//   exports: [MatButtonModule, MatCheckboxModule],
+// })
+// export class MyOwnCustomMaterialModule { }
